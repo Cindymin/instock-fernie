31a8eb33931600105a352be6ddcbdc45e7e4fe51
Claire Lee <jclairelee@gmail.com>
2022-11-21 14:55:23 -0800

    Create Pages and React-Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/pages/Home";
+import Warehouse from "./components/pages/Warehouse";
+import Inventory from "./components/pages/Inventory";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/warehouse" element={<Warehouses />} />
+            <Route path="/inventory" element={<Inventory />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
