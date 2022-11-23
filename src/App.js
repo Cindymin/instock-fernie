9435b74444cf519a0c16be01ef0805ab2563bfaa
欣欣 <cindy@xinxindeMBP.hitronhub.home>
2022-11-23 01:27:54 -0800

    addwarehouse setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import "./App.css";
+import "./App.scss";
 import Warehouse from "./components/pages/warehouse/Warehouse";
 import Inventory from "./components/pages/inventory/Inventory";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
