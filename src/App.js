559e56d44d961141385d321b353e47eade3fa412
Muskan Kapoor <muskankapoor927@gmail.com>
2022-11-23 23:14:38 -0800

    Created warehouse list component and added styling to it and got its data using axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.scss";
 import Warehouse from "./components/pages/warehouse/Warehouse";
+import EditWarehouse from "./components/editwarehouse/EditWarehouse";
+import DeleteWarehouse from "./components/deleteWarehouse/DeleteWarehouse";
 import Inventory from "./components/pages/inventory/Inventory";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,6 +13,9 @@ function App() {
           <Routes>
 
             <Route path="/" exact element={<Warehouse />} />
+            {/* <Route path="/add-warehouse" element={<AddWarehouse />} */}
+            <Route path="/edit-warehouse" element={<EditWarehouse />} />
+            <Route path="/delete-warehouse" element={<DeleteWarehouse />} /> 
             <Route path="/warehouse" element={<Warehouse />} />
             <Route path="/inventory" element={<Inventory />} />
           </Routes>
--- src/components/pages/warehouse/Warehouse.js
+++ src/components/pages/warehouse/Warehouse.js
@@ -2,12 +2,37 @@ import React from "react";
 import Header from "../../header/Header";
 import Footer from "../../footer/Footer";
 import WarehouseDetails from "../../warehouseDetails/WarehouseDetails";
+import WarehouseList from "../../warehouseList/WarehouseList";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Warehouse() {
+
+  const [WHList,setWhList ] = useState([]);
+
+  useEffect(()=>{
+    const fetchWarehouses= async () => {
+      try {
+        const {data} = await axios.get
+        (`http://localhost:8080/warehouses`);
+        setWhList(data);
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+    fetchWarehouses();
+    },
+   []);
+
+
+
+
+
   return (
     <>
       <Header />
-      <WarehouseDetails className="content" />
+      <WarehouseList WHList={WHList} />
+      {/* <WarehouseDetails className="content" /> */}
       <Footer />
     </>
   );
