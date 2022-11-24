007d01fb7fc73f5220494d8caaf88220d319ead9
Junyue Zhang <daisyzhang0301@gmail.com>
2022-11-23 22:43:08 -0800

    get the warehouse's details infomation by API

--- src/components/pages/warehouse/Warehouse.js
+++ src/components/pages/warehouse/Warehouse.js
@@ -1,14 +1,47 @@
 import React from "react";
-import Header from "../../header/Header";
-import Footer from "../../footer/Footer";
 import WarehouseDetails from "../../warehouseDetails/WarehouseDetails";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const SEARCH_URL = `http://localhost:8080/warehouses`;
+const searchByWarehouseID = (id) =>
+  ` http://localhost:8080/warehouses/${id}/inventories`;
 
 function Warehouse() {
+  const [warehouses, setWarehouse] = useState([]);
+  const [warehouseInventory, setWarehouseInventory] = useState([]);
+  const { warehouseId } = useParams();
+  const warehouseIdToDisplay =
+    warehouseId || "5bf7bd6c-2b16-4129-bddc-9d37ff8539e9";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // get warehouseId data from API
+      const warehouse_data = await axios.get(SEARCH_URL);
+      const warehouses = warehouse_data.data;
+      const getWarehouse = (warehouseId) => {
+        return warehouses.find((warehouse) => warehouse.id === warehouseId);
+      };
+      setWarehouse(getWarehouse(warehouseIdToDisplay));
+
+      // get the warehouseInventory data from API
+      const { data } = await axios.get(
+        searchByWarehouseID(warehouseIdToDisplay)
+      );
+      setWarehouseInventory(data.inventoriesData);
+    };
+
+    fetchData();
+  }, [warehouseIdToDisplay]);
+
   return (
     <>
-      <Header />
-      <WarehouseDetails className="content" />
-      <Footer />
+      <WarehouseDetails
+        warehouses={warehouses}
+        warehouseInventory={warehouseInventory}
+        // warehouseId={warehouseId}
+      />
     </>
   );
 }
