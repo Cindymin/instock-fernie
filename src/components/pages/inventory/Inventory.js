ca5dff1641c7cf59b17d4e6e267aaa033fe3a2f0
Claire Lee <jclairelee@gmail.com>
2022-11-23 21:21:53 -0800

    Add API get request and styling in inventoryList

--- src/components/pages/inventory/Inventory.js
+++ src/components/pages/inventory/Inventory.js
@@ -1,13 +1,32 @@
-import React from "react";
 import InventoryList from "../../inventoryList/InventoryList";
 import Header from "../../header/Header";
 import Footer from "../../footer/Footer";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import DeleteInventory from "../../delteInventory/DeleteInventory";
 
 export default function Inventory() {
+  const [inventory, setInventory] = useState([]);
+
+  const getInventory = () => {
+    axios
+      .get("http://localhost:8080/inventories")
+      .then((res) => {
+        console.log(res.data);
+        setInventory(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getInventory();
+  }, []);
+
   return (
     <>
       <Header />
-      <InventoryList />
+      <InventoryList inventory={inventory} />
       <Footer />
     </>
   );
