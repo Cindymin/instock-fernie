daee3a7112b95cf8cbfbc1a068adfd2ebe902268
Claire Lee <jclairelee@gmail.com>
2022-11-23 23:35:10 -0800

    Add Link to delete page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Warehouse from "./components/pages/warehouse/Warehouse";
 import Inventory from "./components/pages/inventory/Inventory";
+import DeleteInventory from "./components/delteInventory/DeleteInventory";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,10 +10,10 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-
             <Route path="/" exact element={<Warehouse />} />
             <Route path="/warehouse" element={<Warehouse />} />
             <Route path="/inventory" element={<Inventory />} />
+            <Route path="/delete-inventory" element={<DeleteInventory />} />
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/inventoryList/InventoryList.js
+++ src/components/inventoryList/InventoryList.js
@@ -5,6 +5,7 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import less from "../../assets/icons/chevron_right-24px.svg";
+import { Link } from "react-router-dom";
 
 const InventoryList = (props) => {
   const items = props.inventory;
@@ -36,28 +37,48 @@ const InventoryList = (props) => {
       <div className="invList__item">
         <div className="invList__item-bar">
           <div className="invList__item-bar-wrap">
-            <span class="invList__item-bar-text">INVENTORY ITEM</span>
-            <img class="invList__item-bar-icon" src={sortIcon} alt="" />
+            <span class="invList__item-bar-text title-inv">INVENTORY ITEM</span>
+            <img
+              class="invList__item-bar-icon invList__item-bar-icon__inventory"
+              src={sortIcon}
+              alt="sort-icon"
+            />
           </div>
 
           <div className="invList__item-bar-wrap">
-            <span className="invList__item-bar-text">CATEGORY</span>
-            <img class="invList__item-bar-icon" src={sortIcon} alt="" />
+            <span className="invList__item-bar-text title-cat">CATEGORY</span>
+            <img
+              class="invList__item-bar-icon invList__item-bar-icon__category"
+              src={sortIcon}
+              alt="sort-icon"
+            />
           </div>
 
           <div className="invList__item-bar-wrap status">
-            <span className="invList__item-bar-text">STATUS</span>
-            <img class="invList__item-bar-icon" src={sortIcon} alt="" />
+            <span className="invList__item-bar-text title-sta">STATUS</span>
+            <img
+              class="invList__item-bar-icon invList__item-bar-icon__status"
+              src={sortIcon}
+              alt="sort"
+            />
           </div>
 
           <div className="invList__item-bar-wrap  quantity">
-            <span className="invList__item-bar-text">QTY</span>
-            <img class="invList__item-bar-icon" src={sortIcon} alt="" />
+            <span className="invList__item-bar-text title-qty">QTY</span>
+            <img
+              class="invList__item-bar-icon invList__item-bar-icon__qty"
+              src={sortIcon}
+              alt="sort"
+            />
           </div>
 
           <div className="invList__item-bar-wrap  warehouse">
-            <span className="invList__item-bar-text">WAREHOUSE</span>
-            <img class="invList__item-bar-icon" src={sortIcon} alt="" />
+            <span className="invList__item-bar-text title-ware">WAREHOUSE</span>
+            <img
+              class="invList__item-bar-icon invList__item-bar-icon__warehouse"
+              src={sortIcon}
+              alt="sort"
+            />
           </div>
 
           <span className="invList__item-bar-text">ACTIONS</span>
@@ -81,15 +102,19 @@ const InventoryList = (props) => {
               <span className="invList__item-info-category">
                 {item.category}
               </span>
+
               <span className="invList__item-info-status">{item.status}</span>
+
               <span className="invList__item-info-num">{item.quantity}</span>
               <span className="invList__item-info-warehouse">warehouse</span>
               <div className="invList__item-info-actions">
-                <img
-                  className="invList__item-info-actions-imge"
-                  src={deleteIcon}
-                  alt="delete"
-                />
+                <Link to="/delete-inventory">
+                  <img
+                    className="invList__item-info-actions-imge"
+                    src={deleteIcon}
+                    alt="delete"
+                  />
+                </Link>
                 <img
                   className="invList__item-info-actions-imge"
                   src={editIcon}
