8ba95df1dc6c3e4bd1faf6a61fe5f0d9c4a28473
Junyue Zhang <daisyzhang0301@gmail.com>
2022-11-25 20:04:52 -0800

    finished all the delete work and fix the wrong icon  problem in front-end delete component

--- src/components/inventoryList/InventoryList.js
+++ src/components/inventoryList/InventoryList.js
@@ -5,223 +5,294 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import less from "../../assets/icons/chevron_right-24px.svg";
+import close from "../../assets/icons/close-24px.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const InventoryList = (props) => {
   const invt = props.item;
 
+  const [inventoryToDelete, setInventoryToDelete] = useState("");
+  const [inventoryIdToDelete, setInventoryIdToDelete] = useState("");
+
+  // let the delete component pop up
+  function showDeleteConfirm(name, id) {
+    document.getElementById("delete").style.display = "block";
+    document.getElementById("background").style.display = "block";
+    setInventoryToDelete(name);
+    setInventoryIdToDelete(id);
+  }
+
+  // click cancel icon
+  function hideDeleteComponent() {
+    document.getElementById("delete").style.display = "none";
+    document.getElementById("background").style.display = "none";
+  }
+
+  // click delete icon
+  function ConfirmDelete() {
+    axios
+      .delete(`http://localhost:8080/inventories/${inventoryIdToDelete}`)
+      .then((response) => console.log(response.data));
+    window.location = "http://localhost:8080/inventory";
+  }
+
   const items = props.inventory;
   return (
-    <form className="invList">
-      <section className="invList-top">
-        <div className="invList-top-title">
-          <h2 className="invList-top-title invList-top-title__text">
-            Inventory
-          </h2>
-        </div>
-        <div className="invList-top-form invList-top-search">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="invList-top-search__bar"
-          />
-          <img
-            src={searchIcon}
-            alt="searchIcon"
-            className="invList-top-search__img"
-          />
-        </div>
-        <div className="invList-top-form invList-top-btn">
-          <button className="invList-top-btn--blue">+ Add New Item</button>
-        </div>
-      </section>
-
-      <div className="invList__item">
-        <div className="invList__item-bar">
-          <div className="invList__item-bar-wrap">
-            <span class="invList__item-bar-text title-inv">INVENTORY ITEM</span>
+    <>
+      <form className="invList">
+        <section className="invList-top">
+          <div className="invList-top-title">
+            <h2 className="invList-top-title invList-top-title__text">
+              Inventory
+            </h2>
+          </div>
+          <div className="invList-top-form invList-top-search">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="invList-top-search__bar"
+            />
             <img
-              class="invList__item-bar-icon invList__item-bar-icon__inventory"
-              src={sortIcon}
-              alt="sort-icon"
+              src={searchIcon}
+              alt="searchIcon"
+              className="invList-top-search__img"
             />
           </div>
-
-          <div className="invList__item-bar-wrap">
-            <span className="invList__item-bar-text title-cat">CATEGORY</span>
-            <img
-              class="invList__item-bar-icon invList__item-bar-icon__category"
-              src={sortIcon}
-              alt="sort-icon"
-            />
+          <div className="invList-top-form invList-top-btn">
+            <button className="invList-top-btn--blue">+ Add New Item</button>
           </div>
+        </section>
 
-          <div className="invList__item-bar-wrap status">
-            <span className="invList__item-bar-text title-sta">STATUS</span>
-            <img
-              class="invList__item-bar-icon invList__item-bar-icon__status"
-              src={sortIcon}
-              alt="sort"
-            />
-          </div>
-
-          <div className="invList__item-bar-wrap  quantity">
-            <span className="invList__item-bar-text title-qty">QTY</span>
-            <img
-              class="invList__item-bar-icon invList__item-bar-icon__qty"
-              src={sortIcon}
-              alt="sort"
-            />
-          </div>
-
-          <div className="invList__item-bar-wrap  warehouse">
-            <span className="invList__item-bar-text title-ware">WAREHOUSE</span>
-            <img
-              class="invList__item-bar-icon invList__item-bar-icon__warehouse"
-              src={sortIcon}
-              alt="sort"
-            />
-          </div>
-
-          <span className="invList__item-bar-text">ACTIONS</span>
-        </div>
-        {items &&
-          items.map((item) => (
-            <div
-              className="invList__item-info-Tablet
-            "
-              key={item.id}
-            >
-              <Link to={`/inventory/${item.id}`}>
-                <div className="invList__item-info-nameWrap">
-                  <span className="invList__item-info-name">
-                    {item.item_name}
-                  </span>
-                  <img
-                    className="invList__item-info-image"
-                    src={less}
-                    alt="arrowRight"
-                  />
-                </div>
-              </Link>
-              <span className="invList__item-info-category">
-                {item.category}
+        <div className="invList__item">
+          <div className="invList__item-bar">
+            <div className="invList__item-bar-wrap">
+              <span class="invList__item-bar-text title-inv">
+                INVENTORY ITEM
               </span>
-
-              {item.status === "In Stock" && (
-                <div className="invList__item-info-status">
-                  <span className="invList__item-info-status--inStock">
-                    In Stock
-                  </span>
-                </div>
-              )}
-              {item.status === "Out of Stock" && (
-                <div className="invList__item-info-status">
-                  <span className="invList__item-info-status--outOfStock">
-                    Out of Stock
-                  </span>
-                </div>
-              )}
-
-              <span className="invList__item-info-num">{item.quantity}</span>
-              <div className="invList__item-info-actions">
-                <Link to="/delete-inventory">
-                  <img
-                    className="invList__item-info-actions-imge"
-                    src={deleteIcon}
-                    alt="delete"
-                  />
-                </Link>
-                <img
-                  className="invList__item-info-actions-imge"
-                  src={editIcon}
-                  alt="edit"
-                />
-              </div>
+              <img
+                class="invList__item-bar-icon invList__item-bar-icon__inventory"
+                src={sortIcon}
+                alt="sort-icon"
+              />
             </div>
-          ))}
 
-        {items &&
-          items.map((item) => (
-            <>
-              <div className="invList__item-info">
-                <div className="invList__item-info-wrap" key={item.id}>
-                  <div className="invList__item-info-contanier">
-                    <span class="invList__item-info-tag">INVENTORY ITEM</span>
-                    <Link to={`/inventory/${item.id}`}>
-                      <div className="invList__item-info-nameWrap">
-                        <span className="invList__item-info-name">
-                          {item.item_name}
-                        </span>
-                        <img
-                          className="invList__item-info-image"
-                          src={less}
-                          alt="arrowRight"
-                        />
-                      </div>
-                    </Link>
+            <div className="invList__item-bar-wrap">
+              <span className="invList__item-bar-text title-cat">CATEGORY</span>
+              <img
+                class="invList__item-bar-icon invList__item-bar-icon__category"
+                src={sortIcon}
+                alt="sort-icon"
+              />
+            </div>
+
+            <div className="invList__item-bar-wrap status">
+              <span className="invList__item-bar-text title-sta">STATUS</span>
+              <img
+                class="invList__item-bar-icon invList__item-bar-icon__status"
+                src={sortIcon}
+                alt="sort"
+              />
+            </div>
+
+            <div className="invList__item-bar-wrap  quantity">
+              <span className="invList__item-bar-text title-qty">QTY</span>
+              <img
+                class="invList__item-bar-icon invList__item-bar-icon__qty"
+                src={sortIcon}
+                alt="sort"
+              />
+            </div>
+
+            <div className="invList__item-bar-wrap  warehouse">
+              <span className="invList__item-bar-text title-ware">
+                WAREHOUSE
+              </span>
+              <img
+                class="invList__item-bar-icon invList__item-bar-icon__warehouse"
+                src={sortIcon}
+                alt="sort"
+              />
+            </div>
+
+            <span className="invList__item-bar-text">ACTIONS</span>
+          </div>
+          {items &&
+            items.map((item) => (
+              <div
+                className="invList__item-info-Tablet
+            "
+                key={item.id}
+              >
+                <Link to={`/inventory/${item.id}`}>
+                  <div className="invList__item-info-nameWrap">
+                    <span className="invList__item-info-name">
+                      {item.item_name}
+                    </span>
+                    <img
+                      className="invList__item-info-image"
+                      src={less}
+                      alt="arrowRight"
+                    />
                   </div>
+                </Link>
+                <span className="invList__item-info-category">
+                  {item.category}
+                </span>
 
-                  <div className="invList__item-info-contanier">
-                    <span class="invList__item-info-tag">STATUS</span>
-                    {item.status === "In Stock" && (
-                      <div className="invList__item-info-status">
-                        <span className="invList__item-info-status--inStock">
-                          In Stock
-                        </span>
-                      </div>
-                    )}
-                    {item.status === "Out of Stock" && (
-                      <div className="invList__item-info-status">
-                        <span className="invList__item-info-status--outOfStock">
-                          Out of Stock
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                </div>
-
-                <div className="invList__item-info-wrap">
-                  <div className="invList__item-info-contanier">
-                    <span class="invList__item-info-tag">CATEGORY</span>
-
-                    <span className="invList__item-info-category">
-                      {item.category}
+                {item.status === "In Stock" && (
+                  <div className="invList__item-info-status">
+                    <span className="invList__item-info-status--inStock">
+                      In Stock
                     </span>
                   </div>
-
-                  <div className="invList__item-info-contanier">
-                    <span class="invList__item-info-tag">QTY</span>
-                    <span className="invList__item-info-num">
-                      {item.quantity}
+                )}
+                {item.status === "Out of Stock" && (
+                  <div className="invList__item-info-status">
+                    <span className="invList__item-info-status--outOfStock">
+                      Out of Stock
                     </span>
                   </div>
-                </div>
+                )}
 
-                <div className="invList__item-info-wrap">
-                  <div className="invList__item-info-contanier"></div>
-                  <div className="invList__item-info-contanier">
-                    <span class="invList__item-info-tag">mobile</span>
-                    <span className="invList__item-info-num">Manhatten</span>
-                  </div>
-                </div>
+                <span className="invList__item-info-num">{item.quantity}</span>
                 <div className="invList__item-info-actions">
                   <img
                     className="invList__item-info-actions-imge"
                     src={deleteIcon}
                     alt="delete"
+                    onClick={() => showDeleteConfirm(item.item_name, item.id)}
                   />
+
                   <img
                     className="invList__item-info-actions-imge"
                     src={editIcon}
                     alt="edit"
                   />
                 </div>
-              </div>{" "}
-            </>
-          ))}
-      </div>
-    </form>
+              </div>
+            ))}
+
+          {items &&
+            items.map((item) => (
+              <>
+                <div className="invList__item-info">
+                  <div className="invList__item-info-wrap" key={item.id}>
+                    <div className="invList__item-info-contanier">
+                      <span class="invList__item-info-tag">INVENTORY ITEM</span>
+                      <Link to={`/inventory/${item.id}`}>
+                        <div className="invList__item-info-nameWrap">
+                          <span className="invList__item-info-name">
+                            {item.item_name}
+                          </span>
+                          <img
+                            className="invList__item-info-image"
+                            src={less}
+                            alt="arrowRight"
+                          />
+                        </div>
+                      </Link>
+                    </div>
+
+                    <div className="invList__item-info-contanier">
+                      <span class="invList__item-info-tag">STATUS</span>
+                      {item.status === "In Stock" && (
+                        <div className="invList__item-info-status">
+                          <span className="invList__item-info-status--inStock">
+                            In Stock
+                          </span>
+                        </div>
+                      )}
+                      {item.status === "Out of Stock" && (
+                        <div className="invList__item-info-status">
+                          <span className="invList__item-info-status--outOfStock">
+                            Out of Stock
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="invList__item-info-wrap">
+                    <div className="invList__item-info-contanier">
+                      <span class="invList__item-info-tag">CATEGORY</span>
+
+                      <span className="invList__item-info-category">
+                        {item.category}
+                      </span>
+                    </div>
+
+                    <div className="invList__item-info-contanier">
+                      <span class="invList__item-info-tag">QTY</span>
+                      <span className="invList__item-info-num">
+                        {item.quantity}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="invList__item-info-wrap">
+                    <div className="invList__item-info-contanier"></div>
+                    <div className="invList__item-info-contanier">
+                      <span class="invList__item-info-tag">mobile</span>
+                      <span className="invList__item-info-num">Manhatten</span>
+                    </div>
+                  </div>
+                  <div className="invList__item-info-actions">
+                    <img
+                      className="invList__item-info-actions-imge"
+                      src={deleteIcon}
+                      alt="delete"
+                      onClick={() => showDeleteConfirm(item.item_name, item.id)}
+                    />
+                    <img
+                      className="invList__item-info-actions-imge"
+                      src={editIcon}
+                      alt="edit"
+                    />
+                  </div>
+                </div>{" "}
+              </>
+            ))}
+        </div>
+      </form>
+
+      <section id="background" className="background">
+        <section className="delete" id="delete">
+          <div className="delete__icon-wrapper">
+            <img
+              className="delete__icon"
+              src={close}
+              alt="close-icon"
+              onClick={() => hideDeleteComponent()}
+            />
+          </div>
+          <div className="delete__title-wrapper">
+            <h1 className="delete__title">Delete {inventoryToDelete} item?</h1>
+          </div>
+          <div className="delete__text-wrapper">
+            <p className="delete__text">
+              Please confirm that you’d like to delete {inventoryToDelete} from
+              the inventory list. You won’t be able to undo this action.
+            </p>
+          </div>
+          <div className="delete__btn-wrapper">
+            <button
+              className="delete__btn delete__btn delete__btn--cancel"
+              onClick={() => hideDeleteComponent()}
+            >
+              Cancel
+            </button>
+            <button
+              className="delete__btn delete__btn--delete"
+              onClick={() => ConfirmDelete()}
+            >
+              Delete
+            </button>
+          </div>
+        </section>
+      </section>
+    </>
   );
 };
 
